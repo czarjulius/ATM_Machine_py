af5a56d26230ae91d4a1b27289830848be459662
dayogeorge <oladayogeorge17@gmail.com>
2019-11-06 21:30:42 +0100

    extract dataset

---
+++ Day_2/add_func.py
@@ -0,0 +1,8 @@
+    
+user_input = int(input("Enter Number "))
+
+def add_10(user_input):
+    result = user_input + 10
+    return result
+
+print(add_10(user_input))
---
+++ Day_2/functions.py
@@ -0,0 +1,8 @@
+a_list = [4444, 8897, 6340, 9896, 4835, 4324, 10, 6445, 661, 1246, 1000, 7429, 1376, 8121, 647, 1280, 3993, 4881,8668,7665,58,87,432]
+sum_manual = 0
+
+for num in a_list:
+    sum_manual += num
+
+print(sum_manual)
+print(sum(a_list))
